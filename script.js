0943ebdf16dd86ca92bdace82b0370d08c3a54d7
n6767ooo6767-cmyk <n6767ooo6767@gmail.com>
2026-06-29 19:40:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('payButton').addEventListener('click', function() {
+    // Временно поставим сюда ссылку на Google, чтобы проверить, что кнопка работает
+    window.location.href = "https://google.com"; 
+});
